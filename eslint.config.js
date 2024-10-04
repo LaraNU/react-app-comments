2a3da71ac9c49184b555d8eeea623833bddf9f4a
Larisa Novozhilova <kara4516@gmail.com>
2024-10-04 20:45:42 +0300

    feat: add the basic configurations for the project

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const browserGlobals = {
+  window: 'readonly',
+  document: 'readonly',
+  navigator: 'readonly',
+};
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: browserGlobals,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+];
