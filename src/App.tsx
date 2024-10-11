516ca199bbd997956cf0eae1a8ac7cac3c3c7242
Larisa Novozhilova <kara4516@gmail.com>
2024-10-11 17:15:31 +0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ProjectsPage from './pages/ProjectsPage/ProjectsPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ProjectsPage />,
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <ProjectsPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -19,78 +19,92 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="wrapper">
-      <div className={styles.container}>
-        <h2 className={styles.title}>Sign up</h2>
-        <p className={styles.text}>
-          Ready to become part of the exclusive club? Fill in the details below,
-          and let the journey begin!
+    <div className={styles.new}>
+      <div className="wrapper">
+        <div className={styles.container}>
+          <h2 className={styles.title}>Sign up</h2>
+          <p className={styles.text}>
+            Ready to become part of the exclusive club? Fill in the details
+            below, and let the journey begin!
+          </p>
+          <form>
+            <div className={styles.formGroup}>
+              <input
+                className={styles.input}
+                type="text"
+                id="name"
+                placeholder="Name"
+                autoComplete="off"
+              />
+              <label className={styles.label} htmlFor="name">
+                Full Name
+              </label>
+            </div>
+            <div className={styles.formGroup}>
+              <input
+                className={styles.input}
+                type="text"
+                id="email"
+                placeholder="Email Address"
+                autoComplete="off"
+              />
+              <label className={styles.label} htmlFor="name">
+                Email Address
+              </label>
+            </div>
+            <div className={styles.formGroup}>
+              <input
+                className={styles.input}
+                type={passwordType}
+                id="password"
+                placeholder="Password"
+                autoComplete="off"
+              />
+              <label className={styles.label} htmlFor="name">
+                Password
+              </label>
+              <img
+                onClick={() =>
+                  toggleInputType(passwordType, setPasswordType, setEyeIcon)
+                }
+                className={styles.eyeIcon}
+                src={eyeIcon}
+                alt="close-eye"
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <input
+                className={styles.input}
+                type={confirmPasswordType}
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                autoComplete="off"
+              />
+              <label className={styles.label} htmlFor="name">
+                Confirm Password
+              </label>
+              <img
+                onClick={() =>
+                  toggleInputType(
+                    confirmPasswordType,
+                    setConfirmPasswordType,
+                    setEyeIconConfirm
+                  )
+                }
+                className={styles.eyeIcon}
+                src={eyeIconConfirm}
+                alt="close-eye"
+              />
+            </div>
+            <button className={styles.formBtn} type="submit">
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <p className={styles.textLink}>
+          Already have an account? <a href="/login">Login</a>
         </p>
-        <form>
-          <div className={styles.formGroup}>
-            <input
-              type="text"
-              id="name"
-              placeholder="Name"
-              autoComplete="off"
-            />
-            <label htmlFor="name">Full Name</label>
-          </div>
-          <div className={styles.formGroup}>
-            <input
-              type="text"
-              id="email"
-              placeholder="Email Address"
-              autoComplete="off"
-            />
-            <label htmlFor="name">Email Address</label>
-          </div>
-          <div className={styles.formGroup}>
-            <input
-              type={passwordType}
-              id="password"
-              placeholder="Password"
-              autoComplete="off"
-            />
-            <label htmlFor="name">Password</label>
-            <img
-              onClick={() =>
-                toggleInputType(passwordType, setPasswordType, setEyeIcon)
-              }
-              className={styles.eyeIcon}
-              src={eyeIcon}
-              alt="close-eye"
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <input
-              type={confirmPasswordType}
-              id="confirmPassword"
-              placeholder="Confirm Password"
-              autoComplete="off"
-            />
-            <label htmlFor="name">Confirm Password</label>
-            <img
-              onClick={() =>
-                toggleInputType(
-                  confirmPasswordType,
-                  setConfirmPasswordType,
-                  setEyeIconConfirm
-                )
-              }
-              className={styles.eyeIcon}
-              src={eyeIconConfirm}
-              alt="close-eye"
-            />
-          </div>
-          <button className={styles.formBtn} type="submit">
-            Sign Up
-          </button>
-        </form>
       </div>
-      <p className={styles.textLink}>
-        Already have an account? <a href="/login">Login</a>
-      </p>
     </div>
   );
 };
